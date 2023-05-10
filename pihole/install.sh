cab91f95f160f82de9171ff96c002656b7693bb5
Regan Karlewicz <regan@karlewr.net>
2023-05-09 19:54:16 -0700

    Add pihole nginx config, add local homebridge DNS configuration, move pihole to port 8080

--- pihole/install.sh
+++ pihole/install.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/sh
 
+echo "Configuring local DNS for homebridge"
+sudo echo "192.168.0.105\thomebridge" >> /etc/hosts
+
 curl -sSL https://install.pi-hole.net | sudo bash
+
+sudo sed -i 's/server\.port\s*=\s*80/server.port = 8080/' /etc/lighttpd/lighttpd.conf
+sudo service lighttpd restart
